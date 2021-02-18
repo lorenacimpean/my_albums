7cb8ed081835db49e264f196b1c5d38a58407333
lorenacimpean <lorenacimpean@rodeapps.com>
2021-02-18 13:03:44 +0200

    fix header layout

--- app/src/main/java/com/example/myalbums/ui/album_details/HeaderModel.kt
+++ app/src/main/java/com/example/myalbums/ui/album_details/HeaderModel.kt
@@ -2,4 +2,8 @@ package com.example.myalbums.ui.album_details
 
 import com.example.myalbums.models.Album
 
-data class HeaderModel(val album: Album, val photoCount: Int)
+data class HeaderModel(val album: Album, val photoCount: Int) {
+
+    val photoCountString: String
+        get() = photoCount.toString()
+}
