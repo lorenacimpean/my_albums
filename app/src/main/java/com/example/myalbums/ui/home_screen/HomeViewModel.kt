3a721c136986a4ecada4b0e8d202033c4b2f02d1
Anca Mihesan <anca.mihesan@rodeapps.com>
2021-02-18 11:14:10 +0200

    try rx item click

--- app/src/main/java/com/example/myalbums/ui/home_screen/HomeViewModel.kt
+++ app/src/main/java/com/example/myalbums/ui/home_screen/HomeViewModel.kt
@@ -3,6 +3,7 @@ package com.example.myalbums.ui.home_screen
 import androidx.lifecycle.ViewModel
 import com.example.myalbums.models.Album
 import com.example.myalbums.repo.AlbumsRepo
+import com.example.myalbums.utils.RxOnItemClickListener
 import com.example.myalbums.utils.UiModel
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.subjects.PublishSubject
@@ -21,7 +22,7 @@ class HomeViewModel(val input: Input, private val albumsRepo: AlbumsRepo) : View
             }
                 .startWith(Observable.just(UiModel.loading()))
                 .onErrorReturn { UiModel.error(it.localizedMessage) }
-        val albumClicked = input.onAlbumClick.map {
+        val albumClicked = input.onAlbumClick.rx.map {
             return@map it
         }
         Output(albums, albumClicked)
@@ -30,10 +31,12 @@ class HomeViewModel(val input: Input, private val albumsRepo: AlbumsRepo) : View
 }
 
 data class Output(
-        val albumsFetched: Observable<UiModel<List<Album>>>,
-        val albumClicked: Observable<Album>)
+    val albumsFetched: Observable<UiModel<List<Album>>>,
+    val albumClicked: Observable<Album>
+)
 
 data class Input(
-        val onLoadData: PublishSubject<Boolean>,
-        val onAlbumClick: PublishSubject<Album>)
+    val onLoadData: PublishSubject<Boolean>,
+    val onAlbumClick: RxOnItemClickListener<Album>
+)
 
