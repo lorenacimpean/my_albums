8543bd809c6c268d163b7a34dc6972de5edad7cd
lorenacimpean <lorenacimpean@rodeapps.com>
2021-02-23 12:51:09 +0200

    added navigation to activity

--- app/src/main/java/com/example/myalbums/ui/profile_screen/ProfileFragment.kt
+++ app/src/main/java/com/example/myalbums/ui/profile_screen/ProfileFragment.kt
@@ -1,14 +1,18 @@
 package com.example.myalbums.ui.profile_screen
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.*
 import androidx.databinding.DataBindingUtil
 import com.example.myalbums.R
 import com.example.myalbums.databinding.FragmentProfileBinding
 import com.example.myalbums.di.BaseFragment
+import com.example.myalbums.utils.subscribeOnMainThread
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class ProfileFragment : BaseFragment() {
 
+    private val viewModel: ProfileViewModel by viewModel<ProfileViewModel>()
     private lateinit var binding: FragmentProfileBinding
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
@@ -16,7 +20,18 @@ class ProfileFragment : BaseFragment() {
     ): View {
         super.enableBackButton()
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_profile, container, false)
+        binding.listener = viewModel.input.openContactInfo
         return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        disposeLater(viewModel.output.onContactInfoTapped.subscribeOnMainThread { _ ->
+            activity?.let {
+                val intent = Intent(it, ContactDetailsActivity::class.java)
+                it.startActivity(intent)
+            }
+        })
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
