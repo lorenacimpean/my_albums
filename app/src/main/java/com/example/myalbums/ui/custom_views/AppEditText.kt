ee9cbf11bacba522db28db344783d24d7a166429
lorenacimpean <lorenacimpean@rodeapps.com>
2021-02-24 15:06:40 +0200

    finished ui

---
+++ app/src/main/java/com/example/myalbums/ui/custom_views/AppEditText.kt
@@ -0,0 +1,39 @@
+package com.example.myalbums.ui.custom_views
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.LinearLayout
+import androidx.databinding.DataBindingUtil
+import com.example.myalbums.R
+import com.example.myalbums.databinding.LayoutAppEditTextBinding
+
+class AppEditText(context: Context, attrs: AttributeSet) : LinearLayout(context, attrs) {
+
+    private val binding: LayoutAppEditTextBinding
+
+    init {
+        val inflater: LayoutInflater = LayoutInflater.from(context)
+        binding = DataBindingUtil.inflate(inflater, R.layout.layout_app_edit_text, this, true)
+    }
+
+    fun setLabel(label: String?) {
+        binding.label = label
+    }
+
+//    fun setText(input: String?) {
+//        binding.input = input
+//    }
+
+    fun setInputType(inputType: Int?) {
+        binding.inputType = inputType
+    }
+
+    fun setError(error: String?) {
+        binding.defaultTextInputLayout.error = error
+    }
+
+    fun getText(): String {
+        return binding.defaultEditText.text.toString()
+    }
+}
--- app/src/main/java/com/example/myalbums/ui/profile_screen/ContactDetailsActivity.kt
+++ app/src/main/java/com/example/myalbums/ui/profile_screen/ContactDetailsActivity.kt
@@ -1,13 +1,18 @@
 package com.example.myalbums.ui.profile_screen
 
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.example.myalbums.R
+import com.example.myalbums.databinding.ActivityContactDetailsBinding
 import com.example.myalbums.di.DisposableActivity
 
 class ContactDetailsActivity : DisposableActivity() {
 
+    private lateinit var binding: ActivityContactDetailsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_contact_details)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_contact_details)
+        setSupportActionBar(binding.toolbarLayout.toolbar)
+
     }
 }
