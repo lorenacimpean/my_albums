be629e28f8ac031580f3dc45dd301c212deaa150
lorenacimpean <lorenacimpean@rodeapps.com>
2021-03-05 14:08:13 +0200

    added ui and data model

---
+++ app/src/main/java/com/example/myalbums/models/User.kt
@@ -0,0 +1,18 @@
+package com.example.myalbums.models
+
+import com.google.gson.annotations.SerializedName
+
+data class User(
+    @SerializedName("id")
+    val id : Int,
+    @SerializedName("name")
+    val name : String,
+    @SerializedName("username")
+    val username : String,
+    @SerializedName("email")
+    val email : String
+) {
+
+    val userIdString : String
+        get() = id.toString()
+}
