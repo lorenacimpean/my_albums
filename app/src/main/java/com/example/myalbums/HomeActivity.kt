65e72fdc691f2de431455f8c59c0a868d7db386d
lorenacimpean <lorenacimpean@rodeapps.com>
2021-03-10 16:57:26 +0200

    finished setup for one signal

--- app/src/main/java/com/example/myalbums/HomeActivity.kt
+++ app/src/main/java/com/example/myalbums/HomeActivity.kt
@@ -4,16 +4,18 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
+import androidx.navigation.ui.*
 import com.example.myalbums.databinding.ActivityHomeBinding
+import com.onesignal.OneSignal
 
 class HomeActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityHomeBinding
-    override fun onCreate(savedInstanceState: Bundle?) {
+    private lateinit var binding : ActivityHomeBinding
+    override fun onCreate(savedInstanceState : Bundle?) {
         super.onCreate(savedInstanceState)
+        OneSignal.setLogLevel(OneSignal.LOG_LEVEL.VERBOSE, OneSignal.LOG_LEVEL.NONE)
+        OneSignal.initWithContext(this)
+        OneSignal.setAppId(BuildConfig.ONE_SIGNAL_KEY)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_home)
         setupBottomNavigation()
     }
@@ -22,16 +24,14 @@ class HomeActivity : AppCompatActivity() {
         val navigationController = findNavController(R.id.navigationHostFragment)
         binding.bottomNavView.setupWithNavController(navigationController)
         val appBarConfiguration = AppBarConfiguration(
-            topLevelDestinationIds = setOf(
-                R.id.homeFragment,
-                R.id.friendsFragment,
-                R.id.newsFragment,
-                R.id.profileFragment
-            )
+                topLevelDestinationIds = setOf(
+                        R.id.homeFragment,
+                        R.id.friendsFragment,
+                        R.id.newsFragment,
+                        R.id.profileFragment
+                )
         )
         setSupportActionBar(binding.toolbarLayout.toolbar)
         setupActionBarWithNavController(navigationController, appBarConfiguration)
     }
-
-
 }
